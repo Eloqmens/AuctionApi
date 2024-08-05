8fdb8c03907a5fac98e9aca9e49e06e92896a918
Eloqmens <mesutovmuhammed@gmail.com>
2024-08-05 23:58:32 +0300

    Add command create for lot

---
+++ Application/Commands/Lot/Create/CreateLotCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using Infrastructure.Data;
+using Core.Entities;
+using MediatR;
+
+namespace Application.Commands.Lot.Create
+{
+    public class CreateLotCommandHandler : IRequestHandler<CreateLotCommand, int>
+    {
+        private readonly AppDbContext _context;
+
+        public CreateLotCommandHandler(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<int> Handle(CreateLotCommand request, CancellationToken cancellationToken)
+        {
+            var lot = new Core.Entities.Lot
+            {
+                Title = request.Title,
+                Description = request.Description,
+                StartingPrice = request.StartingPrice,
+                EndTime = request.EndTime,
+                CategoryId = request.CategoryId,
+                UserId = request.UserId
+            };
+
+            _context.Lots.Add(lot);
+            await _context.SaveChangesAsync();
+            return lot.Id;
+        }
+    }
+}
---
+++ Application/Commands/Lot/Create/CreateLotCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.Lot.Create
+{
+    public class CreateLotCommandValidator : AbstractValidator<CreateLotCommand>
+    {
+        public CreateLotCommandValidator()
+        {
+            RuleFor(x => x.Title).NotEmpty().WithMessage("Title is required.");
+            RuleFor(x => x.Description).NotEmpty().WithMessage("Description is required.");
+            RuleFor(x => x.StartingPrice).GreaterThan(0).WithMessage("Starting price must be greater than 0.");
+            RuleFor(x => x.EndTime).GreaterThan(DateTime.Now).WithMessage("End time must be in the future.");
+            RuleFor(x => x.CategoryId).GreaterThan(0).WithMessage("Category ID is required.");
+            RuleFor(x => x.UserId).NotEmpty().WithMessage("User ID is required.");
+        }
+    }
+}
