c5970483acde0586ea1f34318b0936e449a43197
Eloqmens <mesutovmuhammed@gmail.com>
2024-08-06 03:34:35 +0300

    Add controller for bid and category

---
+++ Application/Commands/User/Register/RegisterUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Commands.User.Register
+{
+    public class RegisterUserCommand : IRequest
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+    }
+}
