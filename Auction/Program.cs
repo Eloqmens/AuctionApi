0d07b5699e68c9329e00d85bfae1e16f04886805
Eloqmens <mesutovmuhammed@gmail.com>
2024-08-23 10:00:21 +0300

    Refactoring

--- Auction/Program.cs
+++ Auction/Program.cs
@@ -1,176 +1,20 @@
-using Application.Behaviors;
-using Application.Commands.Category.Create;
-using Application.Commands.Category.Delete;
-using Application.Commands.Lot.Create;
-using Application.Commands.Lot.Delete;
-using Application.Interfaces;
-using Application.Mappings;
-using Application.Queries.Lot.GetAll;
 using Auction;
-using Auction.Services;
-using Core.Entities;
-using FluentValidation;
-using IdentityServer4.Models;
-using Infrastructure.Data;
-using Infrastructure.Identity;
-using MediatR;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using System.Reflection;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
 
-//Add services to the container.
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLserverAuction")));
-
-builder.Services.AddDbContext<AppIdentityDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SQLserverIdentity")));
-
-//builder.Services.AddDbContext<AppIdentityDbContext>(options =>
-//    options.UseInMemoryDatabase("IdentityDb"));
-
-//builder.Services.AddDbContext<AppDbContext>(options =>
-//    options.UseInMemoryDatabase("AuctionDb"));
-
-builder.Services.AddIdentity<AppUser, IdentityRole>()
-    .AddEntityFrameworkStores<AppIdentityDbContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddIdentityServer()
-    .AddInMemoryApiScopes(new List<ApiScope> { new ApiScope("api1", "My API") })
-    .AddInMemoryClients(new List<Client>
-    {
-        new Client
-        {
-            ClientId = "client",
-            AllowedGrantTypes = GrantTypes.ClientCredentials,
-            ClientSecrets = { new Secret("secret".Sha256()) },
-            AllowedScopes = { "api1" }
-        }
-    })
-    .AddDeveloperSigningCredential();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))   
-    };
-});
-
-builder.Services.AddSingleton<ICurrentUserService, CurrentUserService>();
-builder.Services.AddHttpContextAccessor();
-
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly,
-    typeof(GetLotsQueryHandler).Assembly));
-
-builder.Services.AddAutoMapper(cfg =>
-{
-    cfg.AddProfile(new AssemblyMappingProfile(Assembly.GetExecutingAssembly()));
-    cfg.AddProfile(new AssemblyMappingProfile(typeof(AppDbContext).Assembly));
-});
-
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-
-
-
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Auction API", Version = "v1" });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "Bearer"
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type = ReferenceType.SecurityScheme,
-                    Id = "Bearer"
-                }
-            },
-            new string[] { }
-        }
-    });
-});
-
-static async Task CreateAdminUser(IServiceProvider serviceProvider)
-{
-    var userManager = serviceProvider.GetRequiredService<UserManager<AppUser>>();
-    var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-
-    string adminEmail = "admin@example.com";
-    string adminPassword = "Admin@1234";
-
-    if (!await roleManager.RoleExistsAsync("Admin"))
-    {
-        await roleManager.CreateAsync(new IdentityRole("Admin"));
-    }
-
-    var adminUser = await userManager.FindByEmailAsync(adminEmail);
-    if (adminUser == null)
-    {
-        var newAdmin = new AppUser
-        {
-            UserName = adminEmail,
-            Email = adminEmail,
-            EmailConfirmed = true
-        };
-
-        var result = await userManager.CreateAsync(newAdmin, adminPassword);
-        if (result.Succeeded)
-        {
-            await userManager.AddToRoleAsync(newAdmin, "Admin");
-        }
-        else
-        {
-            throw new ApplicationException("Failed to create admin user.");
-        }
-    }
-}
+// Регистрация сервисов
+ServiceConfiguration.ConfigureServices(builder.Services, builder.Configuration);
 
 var app = builder.Build();
 
-
-
-await SeedData.InitializeAsync(app.Services);
-
+await ServiceConfiguration.InitializeAsync(app.Services);
 
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
-    await CreateAdminUser(services);
+    await ServiceConfiguration.CreateAdminUser(services);
 }
 
 // Configure the HTTP request pipeline.
@@ -185,7 +29,7 @@ app.UseHttpsRedirection();
 app.UseCors(x => x
     .AllowAnyMethod()
     .AllowAnyHeader()
-    .SetIsOriginAllowed(origin => true) 
+    .SetIsOriginAllowed(origin => true)
     .AllowCredentials());
 
 app.UseAuthentication();
